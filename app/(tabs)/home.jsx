9f6511fa50ef37a0e15ad2e97f8715a17c4a6707
shubh <shubhamchauhan244@gmail.com>
2024-07-22 00:30:35 +0530

    first commit

---
+++ app/(tabs)/home.jsx
@@ -0,0 +1,22 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const Home = () => {
+  return (
+ 
+        <View className='h-2/4 w-full bg-indigo-950 rounded-br-full'>
+                <Text className="mt-4"></Text>
+        
+       <Text className="text-white mt-12 p-8 text-6xl font-pbold">Home</Text> 
+
+
+       {/* <Text className="w-24 h-24 rounded-lg bg-white shadow-md"></Text> */}
+    </View>
+   
+
+    
+  )
+}
+
+export default Home
---
+++ constants/images.js
@@ -0,0 +1,18 @@
+import profile from "../assets/images/profile.png";
+import thumbnail from "../assets/images/thumbnail.png";
+import cards from "../assets/images/cards.png";
+import path from "../assets/images/path.png";
+import logo from "../assets/images/logo.png";
+import logoSmall from "../assets/images/logo-small.png";
+import empty from "../assets/images/empty.png";
+import start from '../assets/images/start.png';
+
+import home from '../assets/images/home.png';
+import hospital from '../assets/images/hospital.png';
+import redribbon from '../assets/images/redribbon.png';
+
+import user from '../assets/images/user.png';
+import password from '../assets/images/password.png';
+
+
+export default { profile, thumbnail, cards, path, logo, logoSmall, empty, start, home, hospital, redribbon, user, password };
